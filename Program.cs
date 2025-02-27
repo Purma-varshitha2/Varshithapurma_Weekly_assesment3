e8b12704dcd6045233f201298449759ed3a85c0a
Purma-varshitha2 <purmavarshitha702@gmail.com>
2025-02-27 21:46:06 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,9 @@ class Program
 {
     static void Main()
     {
-        Bank acc1 = new Bank("Ajay", 10000);
-        Bank acc2 = new Bank("Bunny", 15000);
+        SecuritySystem system = new SecuritySystem("Main Gate");
 
-        Console.WriteLine("Initial Interest Rate for All Accounts:");
-        acc1.DisplayAccountDetails();
-        acc2.DisplayAccountDetails();
-
-        
-        Bank.SetInterestRate(4.5);
-
-        Console.WriteLine("\nAfter Changing Interest Rate:");
-        acc1.DisplayAccountDetails();
-        acc2.DisplayAccountDetails();
+      
+        system.PerformSecurityCheck();
     }
 }
-
---
+++ SecuritySystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public sealed class SecuritySystem
+{
+    public string SystemName { get; set; }
+
+    public SecuritySystem(string name)
+    {
+        SystemName = name;
+    }
+
+    public void PerformSecurityCheck()
+    {
+        Console.WriteLine($"Performing security check for {SystemName}...");
+    }
+}
