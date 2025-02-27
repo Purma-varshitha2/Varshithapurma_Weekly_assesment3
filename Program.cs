88556cd529cbf3eed9b0e68d8f0a659f2b273397
Purma-varshitha2 <purmavarshitha702@gmail.com>
2025-02-27 21:35:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main()
     {
-        User admin = new Admin("Vinny");
-        User customer = new Customer("Ram");
-        admin.AccessControl(); 
-        customer.AccessControl();
+    
+        ComplexNumber num1 = new ComplexNumber(1, 2);
+        ComplexNumber num2 = new ComplexNumber(3, 4);
+
+        ComplexNumber result = num1 + num2;
+
+        Console.WriteLine($"First Complex Number: {num1}");
+        Console.WriteLine($"Second Complex Number: {num2}");
+        Console.WriteLine($"Sum: {result}");
     }
 }
-
