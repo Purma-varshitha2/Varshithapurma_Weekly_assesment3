a0ce3f5767648b6e3da925e6f77ab98c1598db90
Purma-varshitha2 <purmavarshitha702@gmail.com>
2025-02-27 22:10:55 +0530

    Add files via upload

---
+++ INotificationObserver.cs
@@ -0,0 +1,5 @@
+﻿public interface INotificationObserver
+{
+    void Update(string message);
+}
+
--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Fetching ConfigurationManager Instance...");
+      
+        NotificationService notificationService = new NotificationService();
+
+       
+        INotificationObserver emailObserver = new EmailNotifier("varshithapurma@gmail.com");
+        INotificationObserver smsObserver = new SMSNotifier("+9763647278");
+
+      
+        notificationService.Subscribe(emailObserver);
+        notificationService.Subscribe(smsObserver);
 
         
-        Parallel.Invoke(
-            () => { ConfigurationManager config1 = ConfigurationManager.Instance; },
-            () => { ConfigurationManager config2 = ConfigurationManager.Instance; }
-        );
+        Console.WriteLine("Sending notification...");
+        notificationService.NotifyAll("Your order has been shipped!");
 
-        ConfigurationManager config = ConfigurationManager.Instance;
-        Console.WriteLine(config.GetConfigValue("AppTheme"));
+        
+        notificationService.Unsubscribe(smsObserver);
+        Console.WriteLine("\nSending another notification...");
+        notificationService.NotifyAll("New promotional offer available!");
     }
 }
